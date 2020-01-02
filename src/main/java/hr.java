e1f37414a1c0c4063662e2fecb3fd269fbf2fd11
백민지 <44864727+bigand32@users.noreply.github.com>
2020-01-02 09:45:12 +0900

    Update hr.java

--- src/main/java/hr.java
+++ src/main/java/hr.java
@@ -23,6 +23,24 @@ public class hr {
 	        }
 	        return fn;
 		}
+	 public static List<Integer> gradingStudents2(List<Integer> grades) {
+		 ArrayList<Integer> fn = new ArrayList<>();
+       		 int score=0;
+        	for (int i=0; i<grades.size();i++){
+            if(grades.get(i)<38){
+                score=grades.get(i);
+            }else{
+                int temp=5-grades.get(i)%5;
+                if(temp<3){
+                    score=grades.get(i)+temp;
+                }else{
+                    score=grades.get(i);
+                }
+            }
+            fn.add(score);
+        }
+        return fn;
+    }
 	}
 
 
