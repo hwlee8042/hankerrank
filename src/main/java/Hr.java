c9feb208e4d91e7d74311f51478eb7fae5851a10
bigand32 <bigand32@gmail.com>
2020-01-08 02:10:04 +0900

    캥거루 알고리즘

--- src/main/java/Hr.java
+++ src/main/java/Hr.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class hr {
+public class Hr {
 	public static List<Integer> gradingStudents(List<Integer> grades) {
-	    // Write your code here
 			ArrayList<Integer> fn = new ArrayList<>();
 	        int score=0;
 	        for (int i = 0; i<grades.size();i++){
@@ -22,7 +21,28 @@ public class hr {
 	         fn.add(score);
 	        }
 	        return fn;
-		}
 	}
+	//
+	  public static String kangaroo(int x1, int v1, int x2, int v2) {
+	      	String result="";
+	        int idx=0;
+	        if(v2>v1){
+	            result="NO";
+	        }else{
+	            while(x2 > x1 && idx<=10000){
+	                idx++;
+	                if(x1+v1*idx == x2+v2*idx){
+	                   result= "YES";
+	                   break;
+	                }
+	                result="NO";
+	            }
+	        }
+	        return result;
+	    }
+}
+
+	  
+	
 
 
