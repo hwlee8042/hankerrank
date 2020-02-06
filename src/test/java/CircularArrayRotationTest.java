acf580daff91c3612659af757c278063b4aac987
백민지 <44864727+bigand32@users.noreply.github.com>
2020-02-06 09:10:52 +0900

    Update CircularArrayRotationTest.java

--- src/test/java/CircularArrayRotationTest.java
+++ src/test/java/CircularArrayRotationTest.java
@@ -7,12 +7,12 @@ import org.junit.Test;
 public class CircularArrayRotationTest {
 	@Test
 	public void testOne(){
-	CircularArraRotation circularArrayRotation = new CircularArraRotation();
+	CircularArraRotation ArrayRotation = new CircularArraRotation();
  
         int[] a = {1, 2, 3};
         int[] queries = {0, 1, 2};
         int[] expect = {2, 3, 1};
-        int[] result = circularArrayRotation.circularArrayRotation(a, 2, queries);
+        int[] result = ArrayRotation.circularArrayRotation(a, 2, queries);
 
         assertThat(expect, is(result));
    }
